08fe80b60c31a4e93fc588312e9f44dc345500e5
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-06-06 15:50:51 +0300

    added auth

--- config/auth.php
+++ config/auth.php
@@ -65,15 +65,15 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => App\User::class,
-        ],
+      //  'users' => [
+       //     'driver' => 'eloquent',
+         //   'model' => App\Models\VRUsers::class,
+      //  ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+         'users' => [
+             'driver' => 'eloquent',
+             'model' => App\Models\VRUsers::class,
+         ],
     ],
 
     /*
