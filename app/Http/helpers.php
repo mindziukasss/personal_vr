c77d26e3928aa9d7cbfd74bc07a03a7589f2d841
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-06-15 10:42:44 +0300

    help function cheking loacele

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -6,6 +6,19 @@ use App\Models\VrLanguageCodes;
 function getActiveLanguages()
 {
     $languages = VrLanguageCodes::where('is_active', 1)->get()->pluck('name', 'id')->toArray();
+    $locale = app()->getLocale();
 
-    return $languages;
+    if (!isset($languages[$locale])) {
+        $locale = config('app.fallback_locale');
+
+        if (!isset($languages[$locale])) {
+            return $languages;
+        }
+
+    }
+
+    $languagesLocale = [$locale => $languages[$locale]] + $languages;
+
+    return $languagesLocale;
+
 }
