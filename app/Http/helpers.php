9aedee6eb70650e8d5266b430ce90e18846e48db
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-06-25 20:49:34 +0300

    fixed helper.php, install new pc

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -7,36 +7,51 @@ use App\Models\VRPages;
 
 function getActiveLanguages()
 {
-    $languages = VrLanguageCodes::where('is_active', 1)->get()->pluck('name', 'id')->toArray();
-    $locale = app()->getLocale();
-
-    if (!isset($languages[$locale])) {
-        $locale = config('app.fallback_locale');
+    if (VrLanguageCodes::class == null) {
+        return "DB is empty";
+    } else {
+        $languages = VrLanguageCodes::where('is_active', 1)->get()->pluck('name', 'id')->toArray();
+        $locale = app()->getLocale();
 
         if (!isset($languages[$locale])) {
-            return $languages;
+            $locale = config('app.fallback_locale');
+
+            if (!isset($languages[$locale])) {
+                return $languages;
+            }
+
         }
 
+        $languagesLocale = [$locale => $languages[$locale]] + $languages;
+
+        return $languagesLocale;
     }
-
-    $languagesLocale = [$locale => $languages[$locale]] + $languages;
-
-    return $languagesLocale;
 
 }
 
 
 function getFrontendMenu ()
-{ 
-  $data = VrMenu::where('vr_parent_id', null)->with(['submenu'])
-      ->orderBy('sequence', 'desc')->get()->toArray();
-    return $data;
+{
+    if(VrMenu::class == null ) {
+        return "DB is empty";
+    }else {
+        $data = VrMenu::where('vr_parent_id', null)->with(['submenu'])
+            ->orderBy('sequence', 'desc')->get()->toArray();
+
+        return $data;
+    }
+
 }
 
 
 function getVRrooms ()
 {
-    $data = VRPages::where('category_id', 'virtual_room')->get()->toArray();
+    if(VRPages::class == null ) {
+        return "DB is empty";
 
-    return $data;
+    }else {
+        $data = VRPages::where('category_id', 'virtual_room')->get()->toArray();
+
+        return $data;
+    }
 }
