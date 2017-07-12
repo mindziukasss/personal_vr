5117a4f72c22e8dc9f2c34c70291200f44a28a52
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-07-12 13:25:52 +0300

    Fixed connPageResources table

---
+++ database/migrations/2017_07_11_165617_drop_column_resource_id_from_vr_connections_pages_resources_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropColumnResourceIdFromVrConnectionsPagesResourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vr_connections_pages_resources', function(Blueprint $table)
+        {
+            $table->dropForeign('fk_vr_connections_users_resources_vr_resources1');
+            $table->dropColumn('resource_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vr_connections_pages_resources', function(Blueprint $table)
+        {
+            $table->string('resource_id', 36);
+            $table->foreign('resource_id', 'fk_vr_connections_users_resources_vr_resources1')->references('id')->on('vr_resources')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+
+        });
+    }
+}
